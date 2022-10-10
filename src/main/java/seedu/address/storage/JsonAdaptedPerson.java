fd022b6e984848d9e5a8e2263512657ecc35a79e
Linus Chui <linuschuii@gmail.com>
2022-10-10 23:37:35 +0800

    Changed test cases to match new constructor in JsonAdaptedPerson

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -65,21 +65,21 @@ class JsonAdaptedPerson {
         }
     }
 
-    /**
-     * Constructs a {@code JsonAdaptedPerson} with the given person details.
-     */
-    @JsonCreator
-    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-                             @JsonProperty("email") String email, @JsonProperty("address") String address,
-                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        if (tagged != null) {
-            this.tagged.addAll(tagged);
-        }
-    }
+//    /**
+//     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+//     */
+//    @JsonCreator
+//    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
+//                             @JsonProperty("email") String email, @JsonProperty("address") String address,
+//                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+//        this.name = name;
+//        this.phone = phone;
+//        this.email = email;
+//        this.address = address;
+//        if (tagged != null) {
+//            this.tagged.addAll(tagged);
+//        }
+//    }
 
     /**
      * Converts a given {@code Person} into this class for Jackson use.
